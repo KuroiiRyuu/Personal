02a4d3761339880a29867ccefbd6fd13c6c4d6dd
Pedro Pérez Montero <pedropm.lolo05@gmail.com>
2024-04-09 19:39:42 +0200

    menu

--- Proyectos/C/Tres_en_RayaV2/tres_en_rayaV2.c
+++ Proyectos/C/Tres_en_RayaV2/tres_en_rayaV2.c
@@ -9,14 +9,53 @@ Fecha : 09 / 04 / 2024
 #include <stdlib.h>
 
 //Estrucutras
-typedef struct{
-    char * nombre;
-}t_jugador;
+typedef enum{
+    salir, crear_partida, continuar_partida
+}t_menu;
 
+typedef struct{
+    char * NameJ1;
+    char * NameJ2;
+    char tablero[3][3];
+}t_partida;
 
 //Cabecera de funciones
+t_menu menu();
 
 int main(int argc, char *argv[]){
+    //Inicializacion de variables
+    t_menu opcion = 0;
+    
+    //Menu
+    do{
+        opcion = menu();
+        switch(opcion){
+            case crear_partida:
+                printf("Creando Partida...\n");
+                break;
+            
+            case continuar_partida:
+                printf("Cargando Archivo...\n");
+                break;
 
+            case salir:
+                printf("Saliendo del Programa...\n");
+                break;
+            
+            default:
+                printf("Opcion Incorrrecta\n");
+        }
+    }while(opcion!= salir);
     return 0;
+}
+
+t_menu menu(){
+    int opcion = 0;
+    printf("---------TIC TAC TOE---------\n");
+    printf("%d - Crear Partida\n", crear_partida);
+    printf("%d - Continuar Partida\n", continuar_partida);
+    printf("%d - Salir\n", salir);
+    printf("------------------------------\n---> ");
+    scanf("%d", &opcion);
+    return opcion;
 }
